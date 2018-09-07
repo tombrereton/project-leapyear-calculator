24031ce3dc1830e659527fff73234ab453349730
Tom Brereton <Tom.Brereton@asos.com>
2018-09-07 11:27:52 +0100

    Add more test for javascript version

--- js/src/leapyear-calculator.js
+++ js/src/leapyear-calculator.js
@@ -1,4 +1,13 @@
 function IsLeapYear(year) {
+    if (year % 400 == 0) {
+        return true;
+    }
+    if (year % 100 == 0) {
+        return false;
+    }
+    if (year % 4 == 0) {
+        return true;
+    }
     return false;
 }
 
--- js/tests/leapyear-calculator.test.js
+++ js/tests/leapyear-calculator.test.js
@@ -8,3 +8,84 @@ test('leap year calculator should identify year one as not a leap year', () => {
 
     expect(actual).toBe(expected);
 });
+
+test('leap year calculator should identify year four as a leap year', () => {
+    let expected = true;
+    let year = 4;
+
+    let actual = IsLeapYear(year)
+
+    expect(actual).toBe(expected);
+});
+
+test('leap year calculator should identify year eight as a leap year', () => {
+    let expected = true;
+    let year = 8;
+
+    let actual = IsLeapYear(year)
+
+    expect(actual).toBe(expected);
+});
+
+test('leap year calculator should identify year twelve as a leap year', () => {
+    let expected = true;
+    let year = 12;
+
+    let actual = IsLeapYear(year)
+
+    expect(actual).toBe(expected);
+});
+
+test('leap year calculator should identify year 100 as not a leap year', () => {
+    let expected = false;
+    let year = 100;
+
+    let actual = IsLeapYear(year)
+
+    expect(actual).toBe(expected);
+});
+
+test('leap year calculator should identify year 200 as not a leap year', () => {
+    let expected = false;
+    let year = 100;
+
+    let actual = IsLeapYear(year)
+
+    expect(actual).toBe(expected);
+});
+
+test('leap year calculator should identify year 300 as not a leap year', () => {
+    let expected = false;
+    let year = 100;
+
+    let actual = IsLeapYear(year)
+
+    expect(actual).toBe(expected);
+});
+
+test('leap year calculator should identify year 400 as a leap year', () => {
+    let expected = true;
+    let year = 400;
+
+    let actual = IsLeapYear(year)
+
+    expect(actual).toBe(expected);
+});
+
+test('leap year calculator should identify year 800 as a leap year', () => {
+    let expected = true;
+    let year = 800;
+
+    let actual = IsLeapYear(year)
+
+    expect(actual).toBe(expected);
+});
+
+test('leap year calculator should identify year 1200 as a leap year', () => {
+    let expected = true;
+    let year = 1200;
+
+    let actual = IsLeapYear(year)
+
+    expect(actual).toBe(expected);
+});
